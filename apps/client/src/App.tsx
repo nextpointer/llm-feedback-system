57aafdcdc34a394e88aab79d12d826f005eae9f4
nextpointer <maitysurajit0901@gmail.com>
2026-07-27 12:41:11 +0530

    feat: project setup done

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button"
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1> hello therwe</h1>
+      <Button variant="outline"> done</Button>
+    </>
+  )
+}
+
+export default App
